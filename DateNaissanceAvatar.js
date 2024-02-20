a2b8ede3a421b50fc9b68d561e045d162440788f
halimgambit <70286759+halimgambit@users.noreply.github.com>
2024-02-20 13:19:57 +0100

    Update DateNaissanceAvatar.js

--- DateNaissanceAvatar.js
+++ DateNaissanceAvatar.js
@@ -2,18 +2,18 @@ exports.action = function(data, callback){
 
 	let client = setClient(data);
 	info("DateNaissanceAvatar from:", data.client, "To:", client);
-	command1 (data, client);
+	avatarNaissance (data, client);
 	callback();
  
 }
 
-function command1 (data, client) {
+function avatarNaissance (data, client) {
 	
 	function age(birthday) {
 		birthday = new Date(birthday)
 		return new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0);
 		}
-		Avatar.speak("j'ai dabord vu le jour en octobre 2017 , j'ai etait développer par l'ingenieur et informaticien : stéphane bascherr , donc j'ai :" + age("1 OCtober 2017") + "ans!", data.client, function(){
+		Avatar.speak(`j'ai dabord vu le jour en octobre 2017 , j'ai etait développer par l'ingenieur et informaticien : stéphane bascherr , donc j'ai :" + ${age('1 OCtober 2017')} ans!`, data.client, () => {
 		Avatar.Speech.end(data.client);
 		});
 }
